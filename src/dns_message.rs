5e8641f02b92a3a13accebfa8f4c30b6a2123c2a
Emmanuel Guefif <eguefif@fastmail.com>
2026-01-17 20:03:11 +0100

    Add dns header struct and return dummy one

---
+++ src/dns_message.rs
@@ -0,0 +1,92 @@
+#![allow(dead_code)]
+use modular_bitfield::prelude::*;
+
+#[bitfield]
+#[derive(Clone)]
+struct HeaderFlags {
+    qr: B1,
+    opcode: B4,
+    aa: B1,
+    tc: B1,
+    rd: B1,
+    ra: B1,
+    reserved: B3,
+    rcode: B4,
+}
+
+struct DNSHeader {
+    id: u16,
+    flags: HeaderFlags,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+impl DNSHeader {
+    pub fn new() -> Self {
+        let flags = HeaderFlags::new()
+            .with_qr(1)
+            .with_opcode(0)
+            .with_aa(0)
+            .with_tc(0)
+            .with_rd(0)
+            .with_ra(0)
+            .with_reserved(0)
+            .with_rcode(0);
+
+        Self {
+            id: 1234,
+            flags,
+            qdcount: 0,
+            ancount: 0,
+            nscount: 0,
+            arcount: 0,
+        }
+    }
+
+    pub fn to_bytes(&self) -> [u8; 12] {
+        let mut response = [0; 12];
+
+        let id = self.id.to_be_bytes();
+        response[0] = id[0];
+        response[1] = id[1];
+
+        let flags = self.flags.clone().into_bytes();
+        response[2] = flags[0];
+        response[3] = flags[1];
+
+        let qdcount = self.qdcount.to_be_bytes();
+        response[4] = qdcount[0];
+        response[5] = qdcount[1];
+
+        let ancount = self.ancount.to_be_bytes();
+        response[6] = ancount[0];
+        response[7] = ancount[1];
+
+        let nscount = self.nscount.to_be_bytes();
+        response[8] = nscount[0];
+        response[9] = nscount[1];
+
+        let arcount = self.arcount.to_be_bytes();
+        response[10] = arcount[0];
+        response[11] = arcount[1];
+
+        response
+    }
+}
+
+pub struct DNSMessage {
+    header: DNSHeader,
+}
+
+impl DNSMessage {
+    pub fn new() -> Self {
+        let header = DNSHeader::new();
+        Self { header }
+    }
+
+    pub fn to_bytes(&self) -> [u8; 12] {
+        return self.header.to_bytes();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use crate::dns_message::DNSMessage;
 #[allow(unused_imports)]
 use std::net::UdpSocket;
+
+pub mod dns_message;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,12 +10,12 @@ fn main() {
 
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
-    
+
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = create_response();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
@@ -23,4 +26,10 @@ fn main() {
             }
         }
     }
+}
+
+fn create_response() -> [u8; 12] {
+    let response = DNSMessage::new();
+
+    return response.to_bytes();
 }
